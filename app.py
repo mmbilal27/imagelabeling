ebc305c4e1416df8b0e8bc2ed3e4f73c3ad9147c
mmbilal27 <58398062+mmbilal27@users.noreply.github.com>
2023-09-10 11:07:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-pip install streamlit Pillow
-# Required Libraries
 import streamlit as st
 from PIL import Image, ImageOps
 import io
+import base64
 
-# Main app function
+# Function to convert image to desired format, resolution, and quality
+def process_image(img, file_format, width, height, quality):
+    img = ImageOps.fit(img, (width, height), method=0, bleed=0.0, centering=(0.5, 0.5))
+    buffer = io.BytesIO()
+    img.save(buffer, format=file_format.upper(), quality=quality)
+    buffer.seek(0)
+    return buffer
+
+# Function to provide a download link for the processed image
+def get_image_download_link(buffer, file_format, label, index):
+    b64 = base64.b64encode(buffer.getvalue()).decode()
+    return f'<a href="data:image/{file_format};base64,{b64}" download="{label}_{index}.{file_format}">Download {label}_{index}.{file_format}</a>'
+
 def main():
     st.title("Label Your Images")
 
-    # Upload images (max 10)
     uploaded_files = st.file_uploader("Choose images", accept_multiple_files=True, type=['jpg', 'jpeg', 'png', 'gif', 'tif', 'svg', 'ico', 'psd', 'pcx'], max_uploaded_files=10)
-
-    # Input field for renaming label
     label = st.text_input("Enter desired label for renaming:")
 
     # Sidebar menu for adjustments
@@ -22,31 +30,17 @@ def main():
         height = st.number_input("Height (pixels)", min_value=1, value=300)
         file_format = st.selectbox("Select file format", ["jpg", "jpeg", "png", "gif"])
 
-    # Check if files uploaded and label is provided
     if uploaded_files and label:
         st.write("Renamed and Adjusted Images:")
-        
+
         for index, uploaded_file in enumerate(uploaded_files):
-            # Convert uploaded file to bytes
             img_data = uploaded_file.read()
             img = Image.open(io.BytesIO(img_data))
             
-            # Adjust image as per sidebar options
-            img = ImageOps.fit(img, (width, height), method=0, bleed=0.0, centering=(0.5, 0.5))
+            img_buffer = process_image(img, file_format, width, height, quality)
             
-            # Convert to desired format
-            buffer = io.BytesIO()
-            img.save(buffer, format=file_format.upper(), quality=quality)
-            buffer.seek(0)
-            
-            # Display and provide download option
             st.image(img, caption=f'{label}_{index+1}.{file_format}', use_column_width=True)
-            st.download_button(
-                label=f"Download {label}_{index+1}.{file_format}",
-                data=buffer,
-                file_name=f"{label}_{index+1}.{file_format}",
-                mime=file_format
-            )
+            st.markdown(get_image_download_link(img_buffer, file_format, label, index+1), unsafe_allow_html=True)
 
     # Social Media Links at the end
     st.write("Connect with me on:")
